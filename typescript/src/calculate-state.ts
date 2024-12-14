5c8b484d0bc441f6ff283ece3f46b76c82789550
Tony Vo <ttrung.vo@gmail.com>
2024-12-14 07:20:30 -0700

    move production code out of test file

---
+++ typescript/src/calculate-state.ts
@@ -0,0 +1,84 @@
+import {CalculatorNumber, CalculatorOperation, MathOperationError, MathOperationResult} from "@/Calculator";
+import {Either as E, Option as O, pipe} from "effect";
+
+type PendingOp = O.Option<[CalculatorOperation, CalculatorNumber]>;
+
+export class CalculatorNonZeroDigit {
+
+    constructor(readonly value: number) {
+    }
+
+    toString(): string {
+        return this.value.toString();
+    }
+}
+
+export interface CalculatorMathOps {
+    readonly op: '+' | '-' | '/' | '*';
+}
+
+interface CalculatorAction {
+    readonly action: 'clear' | 'equal'
+}
+
+export type CalculatorInput =
+    { type: 'zero' }
+    | { type: 'digit', value: CalculatorNonZeroDigit }
+    | { type: 'decimalSeparator' }
+    | { type: 'clear' }
+    | { type: 'equal' }
+    | { type: 'op', value: CalculatorMathOps };
+type CalculatorDisplay = string;
+type ZeroStateData = PendingOp;
+export type DigitAccumulator = string;
+export type AccumulatorStateData = { digit: DigitAccumulator, pendingOp: PendingOp };
+type ComputedStateData = { displayNumber: CalculatorNumber, pendingOp: PendingOp };
+type ErrorStateData = MathOperationError;
+export type CalculatorState =
+    { state: 'zero', value: ZeroStateData }
+    | { state: 'accumulator', value: AccumulatorStateData }
+    | { state: 'accumulatorWithDecimal', value: AccumulatorStateData }
+    | { state: 'computed', value: ComputedStateData }
+    | { state: 'error', value: ErrorStateData };
+type Calculate = (calculatorInput: CalculatorInput, calculatorState: CalculatorState) => CalculatorState;
+
+type AccumulateNonZeroDigit = (nonZeroDigit: CalculatorNonZeroDigit, accumulator: DigitAccumulator) => DigitAccumulator;
+type AccumulateZero = (accumulator: DigitAccumulator) => DigitAccumulator;
+type AccumulateSeparator = (accumulator: DigitAccumulator) => DigitAccumulator;
+type DoMathOperation = (mathOps: CalculatorMathOps, number1: CalculatorNumber, number2: CalculatorNumber) => MathOperationResult;
+type GetNumberFromAccumulator = (accumulatorStateData: AccumulatorStateData) => CalculatorNumber;
+type GetDisplayFromState = (accumulatorStateData: AccumulatorStateData) => string;
+type GetPendingOpsFromState = (accumulatorStateData: AccumulatorStateData) => string;
+export type CalculatorServices = {
+    accumulateNonZeroDigit: AccumulateNonZeroDigit,
+    accumulateZero: AccumulateZero,
+    accumulateSeparator: AccumulateSeparator,
+    doMathOperation: DoMathOperation,
+    getNumberFromAccumulator: GetNumberFromAccumulator,
+    getDisplayFromState: GetDisplayFromState,
+    getPendingOpsFromState: GetPendingOpsFromState
+};
+
+export function createCalculate(calculatorService: CalculatorServices): Calculate {
+    return (input, calculatorState) => {
+        switch (calculatorState.state) {
+            case 'zero':
+                if (input.type === 'digit') {
+                    return {state: "accumulator", value: {digit: input.value.toString(), pendingOp: O.none()}};
+                }
+                if (input.type === 'decimalSeparator') {
+                    return {state: "accumulatorWithDecimal", value: {digit: "0.", pendingOp: O.none()}};
+                }
+                break;
+            case 'accumulator':
+                if (input.type === 'digit') {
+                    return {
+                        state: "accumulator",
+                        value: {digit: calculatorState.value.digit + input.value.toString(), pendingOp: O.none()}
+                    };
+                }
+                break;
+        }
+        return calculatorState;
+    }
+}
--- typescript/test/calculate-state.test.ts
+++ typescript/test/calculate-state.test.ts
@@ -1,101 +1,18 @@
 import {describe, test} from "@jest/globals";
 
-import {CalculatorNumber, CalculatorOperation, MathOperationError, MathOperationResult} from '../src/Calculator';
+import {CalculatorNumber, MathOperationResult} from '../src/Calculator';
 import {Either as E, Option as O} from "effect";
+import {
+    AccumulatorStateData,
+    CalculatorInput,
+    CalculatorMathOps,
+    CalculatorNonZeroDigit,
+    CalculatorServices,
+    CalculatorState,
+    createCalculate,
+    DigitAccumulator
+} from '../src/calculate-state';
 
-
-type PendingOp = O.Option<[CalculatorOperation, CalculatorNumber]>;
-
-class CalculatorNonZeroDigit {
-
-  constructor(readonly value: number) {
-  }
-  toString(): string {
-    return this.value.toString();
-  }
-}
-
-interface CalculatorMathOps {
-  readonly op: '+' | '-' | '/' | '*';
-}
-
-interface CalculatorAction {
-  readonly action: 'clear' | 'equal'
-}
-
-type CalculatorInput =
-    { type: 'zero' }
-    | { type: 'digit', value: CalculatorNonZeroDigit }
-    | { type: 'decimalSeparator'}
-    | { type: 'clear'}
-    | { type: 'equal'}
-    | { type: 'op', value: CalculatorMathOps };
-
-type CalculatorDisplay = string;
-
-type ZeroStateData = PendingOp;
-
-type DigitAccumulator = string;
-
-type AccumulatorStateData = { digit: DigitAccumulator, pendingOp: PendingOp };
-
-type ComputedStateData = { displayNumber: CalculatorNumber, pendingOp: PendingOp };
-
-type ErrorStateData = MathOperationError;
-
-type CalculatorState =
-    { state: 'zero', value: ZeroStateData }
-    | {state: 'accumulator', value: AccumulatorStateData }
-    | {state: 'accumulatorWithDecimal', value: AccumulatorStateData}
-    | {state: 'computed', value: ComputedStateData}
-    | {state: 'error', value: ErrorStateData};
-
-type Calculate = (calculatorInput: CalculatorInput, calculatorState: CalculatorState) => CalculatorState;
-
-function createCalculate(calculatorService: CalculatorServices): Calculate {
-  return (input, calculatorState) => {
-    switch (calculatorState.state) {
-      case 'zero':
-        if (input.type === 'digit') {
-          return {state: "accumulator", value: {digit: input.value.toString(), pendingOp: O.none()}};
-        }
-        if (input.type === 'decimalSeparator') {
-          return { state: "accumulatorWithDecimal", value: { digit: "0.", pendingOp: O.none() } };
-        }
-        break;
-      case 'accumulator':
-        if (input.type === 'digit') {
-          return {state: "accumulator", value: {digit: calculatorState.value.digit + input.value.toString(), pendingOp: O.none()}};
-        }
-        break;
-    }
-    return calculatorState;
-  }
-}
-
-type AccumulateNonZeroDigit = (nonZeroDigit: CalculatorNonZeroDigit, accumulator: DigitAccumulator) => DigitAccumulator;
-
-type AccumulateZero = (accumulator: DigitAccumulator) => DigitAccumulator;
-
-type AccumulateSeparator = (accumulator: DigitAccumulator) => DigitAccumulator;
-
-type DoMathOperation = (mathOps: CalculatorMathOps, number1: CalculatorNumber, number2: CalculatorNumber) => MathOperationResult;
-
-type GetNumberFromAccumulator = (accumulatorStateData: AccumulatorStateData) => CalculatorNumber;
-
-type GetDisplayFromState = (accumulatorStateData: AccumulatorStateData) => string;
-
-type GetPendingOpsFromState = (accumulatorStateData: AccumulatorStateData) => string;
-
-type CalculatorServices = {
-  accumulateNonZeroDigit: AccumulateNonZeroDigit,
-  accumulateZero: AccumulateZero,
-  accumulateSeparator: AccumulateSeparator,
-  doMathOperation: DoMathOperation,
-  getNumberFromAccumulator: GetNumberFromAccumulator,
-  getDisplayFromState: GetDisplayFromState,
-  getPendingOpsFromState: GetPendingOpsFromState
-};
 
 function createServices() {
   const services: CalculatorServices = {
